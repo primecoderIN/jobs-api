5d4fe7bc0def81ac8d70ba41791d598704a378ca
Sanjeev Kumar <sanjeevk@gnrgy.com>
2022-06-04 10:47:27 +0530

    Revert "Removed commented codes"
    This reverts commit 216c40d39182ac39257f1caea0d39f0b877e1278.

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,10 +1,27 @@
 const User = require("../models/user");
 const { StatusCodes } = require("http-status-codes");
+// const bcrypt = require("bcryptjs");
 const { BadRequest } = require("../errors");
+// const jwt = require("jsonwebtoken")
 
 const register = async (req, res) => {
+  // Basic custom validation, we can valdiate this way as well
+  //   const { name, email, password } = req.body;
+  //   if (!name || !email || !password) {
+  //     throw new BadRequest("Please provide valid name, email and password");
+  //   }
+
+  //Example of salting password in controller, we can also has password here
+  //   const { name, email, password } = req.body;
+  //   const salt = await bcrypt.genSalt(10);
+  //   const hashedPassword = await bcrypt.hash(password, salt);
+  //   const tempUser = { name, email, password: hashedPassword };
+  //   const user = await User.create({ ...tempUser });
+
   //Generating token
   const { name, email } = await User.create({ ...req.body });
+  //const token = jwt.sign({UserID: user._id, user: user.name},"jwtsecret", {expiresIn:"10d"}) General way
+
   res
     .status(StatusCodes.CREATED)
     .json({ User: name, Token: User.createJWT(email, name) });
@@ -28,10 +45,12 @@ const login = async (req, res) => {
     throw new BadRequest("Please provide valid password.");
   }
 
-  res.status(StatusCodes.OK).json({
-    name: user[0].name,
-    Token: User.createJWT(user[0].email, user[0].name),
-  });
+  res
+    .status(StatusCodes.OK)
+    .json({
+      name: user[0].name,
+      Token: User.createJWT(user[0].email, user[0].name),
+    });
 };
 
 module.exports = {
