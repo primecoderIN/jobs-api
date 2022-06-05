9043e0a498b328081fc5e514761ad35438c587cc
Sanjeev Kumar <sanjeevk@gnrgy.com>
2022-06-05 12:37:04 +0530

    Created few more api routes for managing jobs

--- controllers/jobs.js
+++ controllers/jobs.js
@@ -1,16 +1,24 @@
 const Job = require("../models/jobs");
 const { StatusCodes } = require("http-status-codes");
-const { BadRequest } = require("../errors");
+const { BadRequest, NotFound } = require("../errors");
+const { findOneAndDelete, findOneAndRemove } = require("../models/jobs");
 
 const getAllJobs = async (req, res) => {
-  const jobs = await Job.find({ createdBy : req.user.UserID }).sort("createdAt");
-  res.status(StatusCodes.OK).json({jobs,TotalRecords: jobs.length});
+  const jobs = await Job.find({ createdBy: req.user.UserID }).sort("createdAt");
+  res.status(StatusCodes.OK).json({ jobs, TotalRecords: jobs.length });
 };
 
-const getJob = async (req, res) => { 
-  const createdBy = req.user.UserID;
-  const jobs = await Job.find({ createdBy });
-  res.status(StatusCodes.OK).json(jobs);
+const getJob = async (req, res) => {
+  const {
+    user: { UserID },
+    params: { id: JobID },
+  } = req;
+
+  const job = await Job.findOne({ _id: JobID, createdBy: UserID });
+  if (!job) {
+    throw new NotFound(`No job found with id ${JobID}`);
+  }
+  res.status(StatusCodes.OK).json({ job });
 };
 const createJob = async (req, res) => {
   req.body.createdBy = req.user.UserID;
@@ -18,10 +26,38 @@ const createJob = async (req, res) => {
   res.status(StatusCodes.CREATED).json(job);
 };
 const updateJob = async (req, res) => {
-  res.send("Update Job");
+  const {
+    body,
+    user: { UserID },
+    params: { id: JobID },
+  } = req;
+  if (!body) {
+    throw new BadRequest("Status can not be empty.");
+  }
+  const job = await Job.findOneAndUpdate(
+    { _id: JobID, createdBy: UserID },
+    body,
+    { new: true, runValidators: true }
+  );
+  if (!job) {
+    throw new NotFound(`No job found with id ${JobID}`);
+  }
+  res.status(StatusCodes.OK).json({ job });
 };
 const deleteJob = async (req, res) => {
-  res.send("Delete Job");
+  const {
+    user: { UserID },
+    params: { id: JobID },
+  } = req;
+  const job = await Job.findOneAndRemove({ _id: JobID, createdBy: UserID });
+  console.log(typeof job);
+  if (!job) {
+    throw new NotFound(`No job found with id ${JobID}`);
+  }
+
+  res
+    .status(StatusCodes.OK)
+    .json({ message: `Job with id ${JobID} deleted successfully.` });
 };
 
 module.exports = {
