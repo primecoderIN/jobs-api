913e3948a933e778d1e89fe3e8e264950f92646f
Sanjeev Kumar <sanjeevk@gnrgy.com>
2022-06-05 15:40:19 +0530

    Modification in error handling

--- middleware/ErrorHandlerMiddleware.js
+++ middleware/ErrorHandlerMiddleware.js
@@ -1,12 +1,23 @@
 const { StatusCodes } = require("http-status-codes");
 const { CustomError } = require("../errors");
 const ErrorHandlerMiddleware = (err, req, res, next) => {
-  if (err instanceof CustomError) {
-    return res.status(err.statusCode).json({ message: err.message });
+
+  let customError = {
+    //setting defaults 
+    statusCode : err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
+    message: err.message || " Something went wrong, please try again later."
+  }
+  // if (err instanceof CustomError) {
+  //   return res.status(customError.statusCode).json({ message: customError.message });
+  // } This is no more needed after modification
+
+  if(err.code && err.code===11000){
+    customError.message= `${err.keyValue.email} is already in use, please try registering with a new email.`
+    customError.statusCode=400; //Cuz it is a bad request 
   }
   return res
-    .status(StatusCodes.INTERNAL_SERVER_ERROR)
-    .json({ message: "Something went wrong, please try again later." });
+    .status(customError.statusCode)
+    .json({ message: customError.message});
 };
 
 module.exports = ErrorHandlerMiddleware;
