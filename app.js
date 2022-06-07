489a385a002b03748daededd6757a712da99c0a5
Sanjeev Kumar <sanjeevk@gnrgy.com>
2022-06-07 22:07:27 +0530

    Implemented basic security features

--- app.js
+++ app.js
@@ -5,12 +5,29 @@ const NotFoundMiddleware = require("./middleware/NotFoundMiddleware");
 const ErrorHandlerMiddleware = require("./middleware/ErrorHandlerMiddleware");
 const AuthorizeUser = require("./middleware/authenticate");
 const connectDB = require("./db/connect");
+//Extra security packages 
+const helmet = require("helmet");
+const cors = require("cors");
+const xss = require("xss-clean")
+const rateLimiter = require("express-rate-limit")
 
 //express app
+
 const app = express();
+
+app.set("trust proxy",1)
+app.use(rateLimiter( {
+	windowMs: 15 * 60 * 1000, // 15 minutes
+	max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+}))
+app.use(helmet())
+app.use(cors())
+app.use(xss())
+
 //routes
 const AuthRoute = require("./routes/auth");
 const JobsRoute = require("./routes/jobs");
+
 //middleware
 app.use(express.json());
 app.use("/api/v1/auth", AuthRoute);
